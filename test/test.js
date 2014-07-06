002993bfa5be0f99dd5183050812d205deb482af
Robin Ricard <ricard.robin@gmail.com>
2014-07-06 15:38:28 +0200

    It Works (tm)

--- test/test.js
+++ test/test.js
@@ -1,15 +1,34 @@
 'use strict';
 
 require('should');
+var fs = require('fs');
+var anyfetchFileHydrater = require('anyfetch-file-hydrater');
+var summary = require('../lib/');
 
-var hydrater = require('../lib/');
-
-
-describe('Test HYDRATER results', function() {
-  it('returns the correct informations', function(done) {
+describe('Test Summary results', function() {
+  it('should summarize an article in english', function(done) {
     var document = {
-      datas: {}
+      title: "Walmart Node.js Memory Leak",
+      metadata: {
+        author: "TJ Fontaine",
+        source: "http://www.joyent.com/blog/walmart-node-js-memory-leak",
+        text: fs.readFileSync(__dirname + "/samples/article-en.txt", {encoding: 'utf8'})
+      }
     };
-    done();
+
+    var changes = anyfetchFileHydrater.defaultChanges();
+
+    summary(null, document, changes, function() {
+      changes.metadata.summary.should.match(/memory leak/i);
+      changes.metadata.summary.should.match(/malloc/i);
+      changes.metadata.summary.should.match(/flamegraph/i);
+      changes.metadata.summary.should.match(/v8 heap/i);
+      changes.metadata.summary.should.match(/handlescope/i);
+
+      changes.metadata.summary.should.not.match(/node apis/i);
+      changes.metadata.summary.should.not.match(/master branch/i);
+      changes.metadata.summary.should.not.match(/josh clulow/i);
+      done();
+    });
   });
 });
